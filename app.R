c5888eed53e9349ffcd59687194597ae8cac3a43
Hamdan Yaccob Kol (Rtd) <113873149+hamdanyc@users.noreply.github.com>
2025-06-30 14:21:21 +0800

    Update app.R

--- app.R
+++ app.R
@@ -1,37 +1,49 @@
+# dashboard for kosmetik
+
 library(shiny)
 library(dplyr)
 
-# Read the dataset
+# read sales data
+df <- readr::read_csv("sales.csv")
 
+# Define UI 
 ui <- fluidPage(
-  titlePanel("Product Sales Dashboard"),  
-  sidebarLayout(
-    sidebarPanel(
-      selectInput(  
-        inputId = "product_choice",
-        label = "Select Product",
-        choices = c("shampoo", "shower gel", "make up cleansing"),
-        selected = "shampoo"
-      ),
-    mainPanel(
-      h2("Total Sales for January"),
-      h3(textOutput("total_amount")
+    titlePanel("Product Sales Dashboard"),  
+    sidebarLayout(
+        sidebarPanel(
+            selectInput(  
+                inputId = "product_choice",
+                label = "Select Product",
+                choices = c("shampoo", "shower gel", "make up cleansing"),
+                selected = "shampoo"
+            ),
+        ),
+            mainPanel(
+                h2("Total Sales for:"),
+                h3(textOutput("total_amount"))
+            )
     )
- )
 )
 
+# server logic
 server <- function(input, output) {
-  output$total_amount <- renderText({
-    selected <- input$product_choice
-    
-    filtered_data <- df %>% filter(item == selected %>% 
-      mutate(total_sales = item_price * qty) %>% 
-      summarise(total = sum(total_sales))
-    
-    total <- filtered_data$total
-    currency_format <- paste0("$", format(round(total, 2), nsmall=2, big.mark=",")))
-    currency_format
-  })
+    output$total_amount <- renderText({
+        selected <- input$product_choice
+        
+        filtered_data <- df %>% filter(item == selected) %>%
+            mutate(total_sales = item_price * qty) %>%
+            summarise(total = sum(total_sales))
+        
+        total <- filtered_data$total
+        currency_format <-
+            paste0("RM", format(
+                round(total, 2),
+                nsmall = 2,
+                big.mark = ","
+            ))
+        currency_format
+    })
 }
 
-shinyApp(ui, server)
+# Run the application 
+shinyApp(ui = ui, server = server)
