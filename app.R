94eb85c15180b56e6423c9ad4147409106cc8059
Hamdan Yaccob Kol (Rtd) <113873149+hamdanyc@users.noreply.github.com>
2025-07-03 10:49:51 +0800

    Update app.R

--- app.R
+++ app.R
@@ -7,69 +7,141 @@ library(mongolite)
 library(ggplot2)
 
 # read sales data
-# uri <- Sys.getenv("URI-1")
-# db <- mongo(collection="sales", db="oa", url=uri)
-# df <- db$find('{}')
-# df_item <- df %>% distinct(item)
+uri <- Sys.getenv("URI")
+db <- mongo(collection="sales", db="oa", url=uri)
+df <- db$find('{}')
 
 dt <- lubridate::today()
 # Define UI 
-ui <- bs4Dash::dashboardPage(
+ui <- dashboardPage(
     title = "Shiny Application",
-    bs4Dash::dashboardHeader(
+    dashboardHeader(
         title = "Wawa Roadmap"
     ),
-    bs4Dash::dashboardSidebar(
-        bs4Dash::sidebarMenu(
-            bs4Dash::menuItem(
+    dashboardSidebar(
+        sidebarMenu(
+            menuItem(
                 text = "Transaksi",
                 tabName = "tab_j1p2rgcpnn"
             ),
-            bs4Dash::menuItem(
-                text = "Ringkasan",
-                tabName = "tab_4dhx95c6et"
+            menuItem(
+                text = "Ringkasan Tahunan",
+                tabName = "tab_inr00usq8d"
+            ),
+            menuItem(
+                text = "Ringkasan Bulanan",
+                tabName = "tab_ijylm47bld"
             )
         )
     ),
-    bs4Dash::dashboardBody(
-        bs4Dash::tabItem(
-            tabName = "tab_j1p2rgcpnn",
-            h1(
-                "Transaksi"
+    dashboardBody(
+        tabItems(
+            tabItem(
+                tabName = "tab_j1p2rgcpnn",
+                h1(
+                    "Transaksi"
+                ),
+                textInput(
+                    inputId = "input_dhnrhjc40z",
+                    label = "Tarikh",
+                    value = dt
+                ),
+                selectInput(
+                    inputId = "input_vp6vjageru",
+                    label = "Kategori",
+                    choices = c("sales retail","sales agent","restock", "misc costs"),
+                    selected = "Sales Retail"
+                ),
+                selectInput(
+                    inputId = "input_tqvu7icwb7",
+                    label = "Item",
+                    choices = c("Sales","Restock FR"),
+                    selected = "Sales"
+                ),
+                numericInput(
+                    inputId = "input_l7ncx2dtfs",
+                    label = "Amaun",
+                    value = 300
+                ),
+                actionButton(
+                    inputId = "input_y48vyz6564",
+                    "Terima!",
+                    class = "btn-success"
+                ),
+                tableOutput(
+                    outputId = "df"
+                )
             ),
-            textInput(
-                inputId = "input_dhnrhjc40z",
-                label = "Tarikh",
-                value = dt
+            tabItem(
+                tabName = "tab_inr00usq8d",
+                h1(
+                    "Ringkasan Tahun 2025"
+                ),
+                tags$p(
+                    "Jualan:"
+                ),
+                textOutput(
+                    outputId = "output_yr_sales"
+                ),
+                tags$p(
+                    "Perbelanjaan:"
+                ),
+                textOutput(
+                    outputId = "output_yr_exp"
+                ),
+                tags$p(
+                    "Pendapatan:"
+                ),
+                textOutput(
+                    outputId = "output_yr_net"
+                ),
+                tags$p(
+                    "Margin:"
+                ),
+                textOutput(
+                    outputId = "output_yr_margin"
+                ),
+                plotOutput(
+                    outputId = "output_waybzvxb4v"
+                )
             ),
-            selectInput(
-                inputId = "input_vp6vjageru",
-                label = "Kategori",
-                choices = c("Sales Retail","Sales Agent","Restock", "Lain-lain"),
-                selected = "Sales Retail"
-            ),
-            selectInput(
-                inputId = "input_tqvu7icwb7",
-                label = "Item",
-                choices = c("Sales","Restock FR"),
-                selected = "Sales"
-            ),
-            numericInput(
-                inputId = "input_l7ncx2dtfs",
-                label = "Amaun",
-                value = 300
-            ),
-            actionButton(
-                inputId = "input_y48vyz6564",
-                "Terima!",
-                class = "btn-success"
-            ),
-            tableOutput(
-                outputId = "df"
+            tabItem(
+                tabName = "tab_ijylm47bld",
+                h1(
+                    "Ringkasan Mengikut Bulan"
+                ),
+                sliderInput(
+                    inputId = "slider_jv8sv5rtdw",
+                    label = "Bulan",
+                    min = 0,
+                    max = 12,
+                    value = 0
+                ),
+                tags$p(
+                    "Jualan:"
+                ),
+                textOutput(
+                    outputId = "output_mth_sales"
+                ),
+                tags$p(
+                    "Perbelanjaan:"
+                ),
+                textOutput(
+                    outputId = "output_mth_exp"
+                ),
+                tags$p(
+                    "Pendapatan:"
+                ),
+                textOutput(
+                    outputId = "output_mth_net"
+                ),
+                tags$p(
+                    "Margin:"
+                ),
+                textOutput(
+                    outputId = "output_mth_margin"
+                )
             )
-        ),
-        bs4Dash::tabItem(
-            tabName = "tab_4dhx95c6et"
         )
     )
 )
@@ -82,6 +154,44 @@ server <- function(input, output) {
             c(input$input_dhnrhjc40z, input$input_vp6vjageru,
               input$input_tqvu7icwb7, input$input_l7ncx2dtfs)
         })
+    })
+    
+    # Calculate total Sales - Exp
+    total_sales <- df %>% 
+        filter(item == "sales retail" | item == "sales agent") %>% 
+        summarise("Total sale" = sum(amount)) %>% unlist()
+    
+    total_exp <- df %>% 
+        filter(item == "restock RF" | item == "misc cost") %>% 
+        summarise("Total sale" = sum(amount)) %>% unlist()
+    
+    net_profit <- total_sales - total_exp
+    net_margin <- net_profit / total_sales * 100
+    output$output_yr_sales <- renderText(total_sales)
+    output$output_yr_exp <- renderText(total_exp)
+    output$output_yr_net <- renderText(net_profit)
+    output$output_yr_margin <- renderText(net_margin)
+    
+    # monthly aggregate
+    observeEvent(input$slider_jv8sv5rtdw,{
+        mth_sales <- df %>% 
+            group_by(lubridate::month(date)) %>% 
+            filter(lubridate::month(date) == input$slider_jv8sv5rtdw) %>% 
+            filter(item == "sales retail" | item == "sales agent") %>% 
+            summarise("Total" = sum(amount))
+        
+        mth_exp <- df %>% 
+            group_by(lubridate::month(date)) %>% 
+            filter(lubridate::month(date) == input$slider_jv8sv5rtdw) %>% 
+            filter(item == "restock RF" | item == "misc cost") %>% 
+            summarise("Total" = sum(amount))
+        
+        mth_net <- mth_sales$Total - mth_exp$Total
+        mth_margin <- mth_net / mth_sales$Total * 100
+        output$output_mth_sales <- renderText(mth_sales$Total)
+        output$output_mth_exp <- renderText(mth_exp$Total)
+        output$output_mth_net <- renderText(mth_net)
+        output$output_mth_margin <- renderText(mth_margin)
     })
 }
 
